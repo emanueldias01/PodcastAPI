bd9b41247cc1be528100e9028584fa322365cde9
emanueldias01 <emanueldias01dev@gmail.com>
2025-07-20 10:56:05 -0300

    create server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("hello world");
+import * as http from 'http'
+
+const port = process.env.PORT;
+
+const server = http.createServer((req : http.IncomingMessage, res : http.ServerResponse) => {})
+
+server.listen(port, () => {
+    console.log(`Server running in port ${port}`);
+});
