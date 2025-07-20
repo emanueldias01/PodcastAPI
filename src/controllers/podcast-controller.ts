0f647e5d5b2dcafcd30f55d85a2b3b19ebbeed80
emanueldias01 <emanueldias01dev@gmail.com>
2025-07-20 11:26:45 -0300

    create service

--- src/controllers/podcast-controller.ts
+++ src/controllers/podcast-controller.ts
@@ -1,25 +1,11 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { serviceListPodcast } from "../services/list-episodes-service";
 
-export const getListPodcasts = async(req : IncomingMessage, res : ServerResponse) => {
+export const getListPodcasts = async(
+    req : IncomingMessage,
+    res : ServerResponse
+) => {
+    const list = await serviceListPodcast();
     res.writeHead(200, { "Content-Type" : "application-json" })
-    res.end(JSON.stringify([
-        {
-            podcastName : "flow",
-            episode : "CBUM - Flow #319",
-            videoId : "pQSuQmUfS30",
-            cover: "https://i.ytimg.com/vi/pQSuQmUfS30/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4StKVMSNz3eXH6WV2s3phlBT8kQ",
-            link : "https://www.youtube.com/live/pQSuQmUfS30?si=OY9Su1L_3jqkbb9t",
-            category : ["saude", "bodybuilder"]
-        },
-        {
-            podcastName : "flow",
-            episode : "CBUM - Flow #319",
-            videoId : "pQSuQmUfS30",
-            cover: "https://i.ytimg.com/vi/pQSuQmUfS30/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4StKVMSNz3eXH6WV2s3phlBT8kQ",
-            link : "https://www.youtube.com/live/pQSuQmUfS30?si=OY9Su1L_3jqkbb9t",
-            category : ["saude", "bodybuilder"]
-        }
-
-    ]
-    ))
+    res.end(JSON.stringify(list))
 }
---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,23 @@
+
+
+export const serviceListPodcast = async () => {
+    return [
+        {
+            podcastName : "flow",
+            episode : "CBUM - Flow #319",
+            videoId : "pQSuQmUfS30",
+            cover: "https://i.ytimg.com/vi/pQSuQmUfS30/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4StKVMSNz3eXH6WV2s3phlBT8kQ",
+            link : "https://www.youtube.com/live/pQSuQmUfS30?si=OY9Su1L_3jqkbb9t",
+            category : ["saude", "bodybuilder"]
+        },
+        {
+            podcastName : "flow",
+            episode : "CBUM - Flow #319",
+            videoId : "pQSuQmUfS30",
+            cover: "https://i.ytimg.com/vi/pQSuQmUfS30/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4StKVMSNz3eXH6WV2s3phlBT8kQ",
+            link : "https://www.youtube.com/live/pQSuQmUfS30?si=OY9Su1L_3jqkbb9t",
+            category : ["saude", "bodybuilder"]
+        }
+
+    ]
+}
